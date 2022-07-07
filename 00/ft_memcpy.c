5a5dcf498f71eb9f4242cb6e2f0cdf9d58d3cf1b
Takeshi Okamoto <takokamo@c1r26s1.42tokyo.jp>
2022-07-07 10:26:54 +0900

    first commit 42cursus

---
+++ 00/ft_memcpy.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: takokamo <takokamo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/07 07:21:12 by takokamo          #+#    #+#             */
+/*   Updated: 2022/07/07 08:47:31 by takokamo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*memcpy(void *dest, const void *src, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		*(unsigned char *)(dest + i) = *(unsigned char *)(src + i);
+		i++;
+	}
+	return (dest);
+}
